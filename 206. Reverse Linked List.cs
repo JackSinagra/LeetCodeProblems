cfcb1805894b6670a0d93ce5d0b842441ed4d257
JackSinagra <104109349+JackSinagra@users.noreply.github.com>
2023-04-27 09:52:47 +0800

    Create 206. Reverse Linked List.cs
    completed reverse linked list problem with iterative and recursive methods

---
+++ 206. Reverse Linked List.cs
@@ -0,0 +1,29 @@
+﻿using System;
+//recursive solution
+public class Solution {
+    public ListNode ReverseList(ListNode head, ListNode prev = null) {
+        if(head == null)
+        {
+            return prev;
+        }
+        ListNode next = head.next;
+        head.next = prev;
+        return ReverseList(next, head);
+    }
+}
+//iterative solution
+public class Solution
+{
+    public ListNode ReverseList(ListNode head)
+    {
+        ListNode prev = null;
+        while (head != null)
+        {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+}
