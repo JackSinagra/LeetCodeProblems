3c5f15564c1c3616b226138e7483f2e370006a4c
JackSinagra <104109349+JackSinagra@users.noreply.github.com>
2023-04-30 17:45:09 +0800

    Create 19. Remove Nth Node from End of List.cs

---
+++ 19. Remove Nth Node from End of List.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+public class Solution
+{
+    public ListNode RemoveNthFromEnd(ListNode head, int n)
+    {
+        ListNode headDumby = head;
+        ListNode prev = null;
+        int totalPlace = 0;
+        int currentPlace = 0;
+        while (head != null)
+        {
+            totalPlace++;
+            head = head.next;
+        }
+        head = headDumby;
+        while (head != null)
+        {
+            currentPlace++;
+            if (totalPlace - n + 1 == currentPlace)
+            {
+                if (prev == null)
+                {
+                    return headDumby.next;
+                }
+                if (prev != null && head.next != null)
+                {
+                    prev.next = head.next;
+                    break;
+                }
+                if (head.next == null)
+                {
+                    prev.next = null;
+                    break;
+                }
+            }
+            prev = head;
+            head = head.next;
+        }
+        return headDumby;
+    }
+}
